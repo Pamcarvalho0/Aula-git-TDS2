63b470cde46e5d7cdec3344347d6e402cfb2cdb4
Pamcarvalho0 <pamela.tavares@edu.senai.br>
2026-05-21 14:24:29 -0300

    criei exemplos de array com string e numbers

--- exemplo.js
+++
@@ -1,5 +0,0 @@
-//Pamela Carvalho
-
-//Exemplo de array
-
-let frutas - ["maçã", "banana", "laranja", "uva"];
---
+++ exemplos-array/exemplo.js
@@ -0,0 +1,10 @@
+//Pamela Carvalho
+
+//Exemplo de array
+
+let frutas = ["maçã", "banana", "laranja", "uva"];
+
+console.log(sucos[0]);
+
+//criando um array de números
+let numeros = [1, 2, 3, 4, 5,];
